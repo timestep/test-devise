563e0fc2f88684b97a19939a75a2669db338d3f7
timestep <jbosshellokitty@gmail.com>
2013-08-11 15:24:03 -0400

    initial commit and added devise / generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestDevise::Application.config.secret_key_base = '6b6a07064a0b978270db979ad6f7c336bcf3bf590f8304e43d6c80c4a650289d7911bb842c3e510c6cfddf001120bbd2cdb3f259c99773535e11b0e5f3008014'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestDevise::Application.config.session_store :cookie_store, key: '_test_devise_session'
